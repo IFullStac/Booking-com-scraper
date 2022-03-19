86d4e87eda11aee320adbf8a263ced21f4ac9e20
IFullStac <94450748+IFullStac@users.noreply.github.com>
2022-03-19 02:14:13 +0200

    Update bookings-controllers.js

--- controllers/bookings-controllers.js
+++ controllers/bookings-controllers.js
@@ -33,7 +33,8 @@ const getBookings = (req, res) => {
         allHotels = []
         const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
         const page = await browser.newPage()
-        await page.setViewport({ width: 1000, height: 1600 });
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        //await page.setViewport({ width: 1000, height: 1600 });
         numberOfNights = 2
         numberOfPeople = 2
 
