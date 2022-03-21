1273ad8ff95b255a9d23f8f5510ed4b3df6706e7
Julius <94450748+IFullStac@users.noreply.github.com>
2022-03-21 14:48:28 +0200

    deleted copy

--- Routes/bookings-routes copy.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { getBookings} = require('../controllers/bookings-controllers')
-
-router.get('/:id', getBookings)
-
-module.exports = router
